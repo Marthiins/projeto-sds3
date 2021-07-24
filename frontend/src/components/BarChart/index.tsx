58dcdac2bbba68cd228be59e98727de706108e48
unknown <sergio.marthiins@gmail.com>
2021-07-24 20:15:03 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,8 +1,60 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSucess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/request';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 //function BarChart() { declarar a função de outro modo
 const BarChart = () => { //Essa função é com o Lambda
-    
+
+    const [chartData, setChartData] = useState<ChartData>({ //usar o useState para guardar o Chardata 
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []
+            }
+        ]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sucess-by-seller`)
+            .then(response => { // resposta quando chegar com sucesso
+                const data = response.data as SaleSucess[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1)); //usei o round para format o numero
+
+                setChartData({
+                    labels: {
+                        categories: myLabels
+                    },
+                    series: [
+                        {
+                            name: "% Sucess",
+                            data: mySeries
+                        }
+                    ]
+                });
+            });
+    }, []);
+
+
     const options = {
         plotOptions: {
             bar: {
@@ -10,28 +62,28 @@ const BarChart = () => { //Essa função é com o Lambda
             }
         },
     };
-    
-    const mockData = { //Criar dados de mentiras "dados estaticos"
+
+   /* const mockData = { //Criar dados de mentiras "dados estaticos"
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
         },
         series: [
             {
                 name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]
             }
         ]
-    };
-    
-    
-    return (
-      <Chart 
-      options ={{ ...options, xaxis: mockData.labels}}
-      series={mockData.series} 
-      type="bar"
-      height="240"
+    };*/
 
-      />
+
+    return (
+        <Chart
+            options={{ ...options, xaxis: chartData.labels }}
+            series={chartData.series}
+            type="bar"
+            height="240"
+
+        />
     );
 }
 
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -6,49 +6,49 @@ import { SaleSun } from 'types/sale';
 import { BASE_URL } from 'utils/request';
 
 type ChartData = {
-    series: number [];
-    labels: string [];
- }
+    series: number[];
+    labels: string[];
+}
 
 
 //function DonutChart() { declarar a função de outro modo
 const DonutChart = () => { //Essa função é com o Lambda
-    
-const [chartData, setChartData] = useState<ChartData>({labels:[], series:[]});
 
-useEffect(() =>{
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response =>{ // resposta quando chegar com sucesso
-        const data = response.data as SaleSun[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
-    
-        setChartData({labels:myLabels, series:mySeries});
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => { // resposta quando chegar com sucesso
+                const data = response.data as SaleSun[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-},[]);
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+
+    }, []);
 
 
-  // const mockData = {
-     //   series: [477138, 499928, 444867, 220426, 473088], //Array de numeros
-     //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-   // }
-    
+    /* const mockData = {  //Criar dados de mentiras "dados estaticos"
+       //   series: [477138, 499928, 444867, 220426, 473088], //Array de numeros
+       //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+      }*/
+
     const options = {
         legend: {
             show: true
         }
     }
-    
-    return (
-      <Chart 
-      options ={{ ...options, labels: chartData.labels}}
-      series={chartData.series} 
-      type="donut"
-      height="240"
 
-      />
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+
+        />
     );
 }
 
