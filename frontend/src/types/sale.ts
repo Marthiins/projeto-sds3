539072a86ce87048291add8e3bfb928c4fe629f5
unknown <sergio.marthiins@gmail.com>
2021-07-24 21:12:25 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,12 +1,33 @@
-import { type } from "os"
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = { //busca paginada
+    content?: Sale[]; //? é para indicar como opcional
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean; //quando buscar o true é boolean
+    numberOfElements?: number;
+    empty?: boolean;
+}
 
 export type SaleSun = {
     sellerName: string;
     sum: number;
 }
 
-export type SaleSucess ={
-        sellerName: string;
-        visited: number;
-        deals: number;
+export type SaleSucess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
